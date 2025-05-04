bf9db8bbb3b812c66899b43a5b5d1fac0391fdfb
Wisenk <wisenkmr93@gmail.com>
2025-05-04 16:01:51 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -19,11 +23,28 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to 'bookers/books'
+  end
 
 private
   def book_params
     params.require(:book).permit(:title, :body)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
   get '/bookers' => 'homes#top'
+
   get '/bookers/books' => 'books#new', as: 'newbook'
   post 'newbook' => 'books#new'
+  get 'bookers/books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  post 'edit_book' => 'books#edit'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'bookers/books/:id' => 'books#destroy', as: 'destroy_book'
 
 
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   
-  get 'bookers/books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
